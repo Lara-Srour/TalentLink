b0b2e4b36638f0ed2021c112014f78d3d00322dc
Lara Srour <ls.sroulara@gmail.com>
2025-09-22 22:32:06 +0300

    Add HealthController

---
+++ TalentLink/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TalentLink.Controllers
+{
+    [Route("health")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
